86a1ef32492a170aad09602c12f62ac322de50f5
Roy Plu <roy.plu@hotmail.com>
2018-02-18 20:27:37 +0100

    Last changes to rename Profile to User

--- src/app/models/profile.model.ts
+++ src/app/models/profile.model.ts
@@ -4,13 +4,16 @@ export class Profile {
     bio: string;
     photos: string[];
     photo_optimizer_enabled: boolean;
+    pos: string[]
+    
 
-    constructor(_id: string, name: string, bio: string, photos: string[], photo_optimizer_enabled: boolean,) {
+    constructor(_id: string, name: string, bio: string, photos: string[], photo_optimizer_enabled: boolean, pos: string[]) {
 
         this._id = _id;
         this.name = name;
         this.bio = bio;
         this.photos = photos;
         this.photo_optimizer_enabled = photo_optimizer_enabled;
+        this.pos = pos;
     }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -4,13 +4,15 @@ export class User {
     bio: string;
     photos: string[];
     photo_optimizer_enabled: boolean;
+    pos: string[];
 
-    constructor(_id: string, name: string, bio: string, photos: string[], photo_optimizer_enabled: boolean,) {
+    constructor(_id: string, name: string, bio: string, photos: string[], photo_optimizer_enabled: boolean, pos: string[]) {
 
         this._id = _id;
         this.name = name;
         this.bio = bio;
         this.photos = photos;
         this.photo_optimizer_enabled = photo_optimizer_enabled;
+        this.pos = pos;
     }
 }
