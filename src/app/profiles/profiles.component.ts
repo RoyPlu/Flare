39f34bbb962f4830d9f6ff9b117a495b4861fb97
Roy Plu <roy.plu@hotmail.com>
2018-02-22 18:14:09 +0100

    Reload Page on changing filter

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -10,25 +10,25 @@ import { Profile } from '../models/profile.model';
 })
 export class ProfilesComponent implements OnInit {
 
-exampleStringArray: string[] = null;
-profiles: Profile[];
-profile: Profile = new Profile("1", "Name", "Bio", this.exampleStringArray, false, this.exampleStringArray);
-matchStatus: string = null;
+  exampleStringArray: string[] = null;
+  profiles: Profile[];
+  profile: Profile = new Profile("1", "Name", "Bio", this.exampleStringArray, false, this.exampleStringArray);
+  matchStatus: string = null;
 
-age_filter_min: number;
-age_filter_max: number;
+  age_filter_min: number;
+  age_filter_max: number;
 
-distance_filter: number;
+  distance_filter: number;
 
   constructor(private service: TinderService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getProfiles();
     this.getAgeFilter();
     this.getDistanceFilter();
   }
 
-  getProfiles(){
+  getProfiles() {
     this.service.getProfiles().subscribe(data => {
       console.log(data.data.results);
       this.profiles = data.data.results;
@@ -37,7 +37,15 @@ distance_filter: number;
     this.scrollToTop();
   }
 
-  getProfile(id: string){
+  seePossibleMatches(distance: number) {
+    this.service.setDistanceFilter(distance).subscribe(data => {
+      console.log("Changed distance filter to see possible matches");
+    })
+    this.distance_filter = distance;
+    window.location.reload();
+  }
+
+  getProfile(id: string) {
     console.log(id);
     this.service.getProfile(id).subscribe(data => {
       console.log(data.results);
@@ -45,56 +53,56 @@ distance_filter: number;
     })
   }
 
-  findOnFacebook(name: string, teaser: string){
+  findOnFacebook(name: string, teaser: string) {
     window.open('https://www.facebook.com/search/people/?q=' + name + '%20' + teaser);
   }
 
-  findOnInstagram(username: string){
+  findOnInstagram(username: string) {
     window.open('https://www.instagram.com/' + username);
   }
 
-  likeTinderProfile(id: string, s_number: string){
+  likeTinderProfile(id: string, s_number: string) {
     this.service.likeProfile(id, s_number).subscribe(data => {
       console.log(data.match);
-      if(data.match.following == true){
+      if (data.match.following == true) {
         this.matchStatus = "Yes!";
         this.playNotification();
       } else {
         this.matchStatus = "No";
       }
-      
+
     })
   }
 
-  passTinderProfile(id: string, s_number: string){
+  passTinderProfile(id: string, s_number: string) {
     this.service.passProfile(id, s_number).subscribe(data => {
       console.log(data);
-      if(data.status == 200){
+      if (data.status == 200) {
         this.matchStatus = "Passed";
       }
     })
   }
 
-  superlikeTinderProfile(id: string, s_number: string){
+  superlikeTinderProfile(id: string, s_number: string) {
     this.service.superlikeProfile(id, s_number).subscribe(data => {
       console.log(data.match);
-      if(data.match == true){
+      if (data.match == true) {
         this.matchStatus = "Yes!";
         this.playNotification();
       } else {
         this.matchStatus = "No";
       }
-      
+
     })
   }
 
-  removeFromList(profile, status){
+  removeFromList(profile, status) {
     console.log(status);
     let index = this.profiles.indexOf(profile);
     this.profiles.splice(index, 1)
   }
 
-  getAgeFilter(){
+  getAgeFilter() {
     this.service.getUserProfileV2().subscribe(data => {
       console.log("Age min:" + data.data.user.age_filter_min + ", Age max:" + data.data.user.age_filter_max);
       this.age_filter_min = data.data.user.age_filter_min;
@@ -102,13 +110,15 @@ distance_filter: number;
     })
   }
 
-  setAgeFilter(){
+  setAgeFilter() {
     this.service.setAgeFilter(this.age_filter_min, this.age_filter_max).subscribe(data => {
       console.log("Changed age filter");
     })
+
+    window.location.reload();
   }
 
-  getDistanceFilter(){
+  getDistanceFilter() {
     this.service.getUserProfileV2().subscribe(data => {
       console.log("Distance filter (in miles):" + data.data.user.distance_filter);
       this.distance_filter = data.data.user.distance_filter;
@@ -116,21 +126,23 @@ distance_filter: number;
     })
   }
 
-  setDistanceFilter(){
+  setDistanceFilter() {
     this.service.setDistanceFilter(this.distance_filter).subscribe(data => {
       console.log("Changed distance filter (in miles): " + this.distance_filter);
       console.log("Changed distance filter (in km): " + this.distance_filter * 1.6);
     })
+
+    window.location.reload();
   }
 
-  playNotification(){
+  playNotification() {
     let audio = new Audio();
     audio.src = "../../../assets/audio/soft_notification.mp3";
     audio.load();
     audio.play();
-    }
+  }
 
-  scrollToTop(){
+  scrollToTop() {
     window.scrollTo(0, 0);
   }
 }
