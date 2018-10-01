4fa02e8274756d13838a9b05ce9efe5c75933448
RoyPlu <roy.plu@hotmail.com>
2018-10-01 18:51:48 +0200

    Added AI object detection to separate Profiles
    Using Cloudmersive's image detection API, Project Flare is now able to detect and recognize objects & people in an image and describe them using natural language

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,115 +5,163 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Profile } from '../models/profile.model';
 import { Title } from '@angular/platform-browser';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.css']
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-    exampleStringArray: string[] = null;
-    profile: Profile = new Profile("1", "Name", "Bio", this.exampleStringArray, false, this.exampleStringArray);
+  exampleStringArray: string[] = null;
+  profile: Profile = new Profile("1", "Name", "Bio", this.exampleStringArray, false, this.exampleStringArray);
 
-    id: string;
-    s_number: string;
-    matchStatus: string = null;
+  id: string;
+  s_number: string;
+  matchStatus: string = null;
 
-    browserTitle: string;
+  browserTitle: string;
 
-    superlikesCounter: number;
+  superlikesCounter: number;
 
-    constructor(private titleService: Title, private route: ActivatedRoute, private service: TinderService) { }
+  form: FormGroup;
+  file: File;
+  loading: boolean = false;
+  cloudmersiveData: string = "";
 
-    ngOnInit() {
-        this.route.params.subscribe(params => {
-            console.log(params) //log the entire params object
-            console.log(params['id']) //log the value of id
-            console.log(params['s_number'])
+  constructor(private titleService: Title, private route: ActivatedRoute, private service: TinderService, private fb: FormBuilder) {
+    this.createForm();
+  }
 
-            this.id = params['id'];
-            this.s_number = params['s_number'];
-        });
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params) //log the entire params object
+      console.log(params['id']) //log the value of id
+      console.log(params['s_number'])
 
-        this.getProfile(this.id);
-        this.getSuperlikes();
-    }
+      this.id = params['id'];
+      this.s_number = params['s_number'];
+    });
 
-    getProfile(id: string) {
-        console.log(id);
-        this.service.getProfile(id).subscribe(data => {
-            console.log(data.results);
-            this.profile = data.results;
+    this.getProfile(this.id);
+    this.getSuperlikes();
+  }
 
-            this.setTitle(this.profile.name);
+  createForm() {
+    this.form = this.fb.group({
+      imageFile: null
+    });
+  }
 
-        })
-    }
+  getProfile(id: string) {
+    console.log(id);
+    this.service.getProfile(id).subscribe(data => {
+      console.log(data.results);
+      this.profile = data.results;
 
-    findOnFacebook(name: string, school: string) {
-      window.open('https://www.facebook.com/search/people/?q=' + name + '%20' + school);
-    }
+      this.setTitle(this.profile.name);
 
-    findOnInstagram(username: string) {
-      window.open('https://www.instagram.com/' + username);
-    }
+    })
+  }
 
-    setTitle(browserTitle: string){
-        this.titleService.setTitle( browserTitle );
-    }
+  findOnFacebook(name: string, school: string) {
+    window.open('https://www.facebook.com/search/people/?q=' + name + '%20' + school);
+  }
 
-    likeTinderProfile() {
-        this.service.likeProfile(this.id, this.s_number).subscribe(data => {
-          console.log(data.match);
-          if (data.match.following == true) {
-            this.matchStatus = "Yes!";
-            this.playNotification();
-          } else {
-            this.matchStatus = "No";
-          }
-    
-        })
-      }
-    
-      passTinderProfile() {
-        this.service.passProfile(this.id, this.s_number).subscribe(data => {
-          console.log(data);
-          if (data.status == 200) {
-            this.matchStatus = "Passed";
-          }
-        })
-      }
-    
-      superlikeTinderProfile() {
-        this.service.superlikeProfile(this.id, this.s_number).subscribe(data => {
-          console.log(data.match);
-          if (data.match == true) {
-            this.matchStatus = "Yes!";
-            this.playNotification();
-          } else {
-            this.matchStatus = "No";
-          }
-    
-        })
+  findOnInstagram(username: string) {
+    window.open('https://www.instagram.com/' + username);
+  }
+
+  setTitle(browserTitle: string) {
+    this.titleService.setTitle(browserTitle);
+  }
+
+  likeTinderProfile() {
+    this.service.likeProfile(this.id, this.s_number).subscribe(data => {
+      console.log(data.match);
+      if (data.match.following == true) {
+        this.matchStatus = "Yes!";
+        this.playNotification();
+      } else {
+        this.matchStatus = "No";
       }
 
-      getSuperlikes(){
-        this.service.getUserProfileV2().subscribe(data => {
-          console.log(data.data.super_likes);
-          this.superlikesCounter = data.data.super_likes.remaining;
-        })
+    })
+  }
+
+  passTinderProfile() {
+    this.service.passProfile(this.id, this.s_number).subscribe(data => {
+      console.log(data);
+      if (data.status == 200) {
+        this.matchStatus = "Passed";
       }
-    
-      lowerSuperlikes(superlike: number){
-        this.superlikesCounter = this.superlikesCounter - 1;
+    })
+  }
+
+  superlikeTinderProfile() {
+    this.service.superlikeProfile(this.id, this.s_number).subscribe(data => {
+      console.log(data.match);
+      if (data.match == true) {
+        this.matchStatus = "Yes!";
+        this.playNotification();
+      } else {
+        this.matchStatus = "No";
       }
 
-      playNotification() {
-        let audio = new Audio();
-        audio.src = "../../../assets/audio/soft_notification.mp3";
-        audio.load();
-        audio.play();
-      }
+    })
+  }
+
+  getSuperlikes() {
+    this.service.getUserProfileV2().subscribe(data => {
+      console.log(data.data.super_likes);
+      this.superlikesCounter = data.data.super_likes.remaining;
+    })
+  }
+
+  lowerSuperlikes(superlike: number) {
+    this.superlikesCounter = this.superlikesCounter - 1;
+  }
+
+  playNotification() {
+    let audio = new Audio();
+    audio.src = "../../../assets/audio/soft_notification.mp3";
+    audio.load();
+    audio.play();
+  }
+
+  imageURLtoFile(imageURL: string) {
+    this.service.getImage(imageURL).subscribe(data => {
+      console.log(data);
+
+      var file = new File([data], "image");
+      console.log(file);
+      this.form.get('imageFile').setValue(file);
+    })
+
+    setTimeout(() => {
+      this.onSubmit();
+    },
+      3000);
+  }
+
+  private prepareSave(): any {
+    let input = new FormData();
+    // This can be done a lot prettier; for example automatically assigning values by looping through `this.form.controls`, but we'll keep it as simple as possible here
+    input.append('imageFile', this.form.get('imageFile').value);
+    return input;
+  }
+
+  onSubmit() {
+    const formModel = this.prepareSave();
+    this.loading = true;
+    // In a real-world app you'd have a http request / service call here like
+    // this.http.post('apiUrl', formModel)
+
+    this.service.cloudmersiveDetect(formModel).subscribe(data => {
+      console.log(data);
+      this.cloudmersiveData = data.BestOutcome.Description;
+    })
+  }
 
 }
